34ca74eb535fab011885d1fc8ac458c8c93eab78
Matt Ocampo <mattocampo17@gmail.com>
2021-12-04 03:00:00 +0800

    Co-authored-by: Kapatid <Kapatid@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Citations
+
+- [Origins](https://www.britannica.com/topic/Spider-Man-comic-book-character)
+- [Abilities](https://www.marvel.com/characters/spider-man-peter-parker/in-comics)
+- [Weakness](https://screenrant.com/spider-man-biggest-weaknesses-didnt-know-superpowers-trivia/)
+- [Notable-Allies](https://screenrant.com/spider-man-superheroes-best-friends-bff-deadpool/)
+- [Notable-Enemies](https://www.sideshow.com/geek/10-greatest-spider-man-villains/)
--- home.js
+++ home.js
@@ -1,32 +1,24 @@
 
-$('#btn').on('click', () =>{
-  
-})
-
-// $('body').css({
-//   backgroundColor: "black"
-// })
-
 $('#spiderman').animate({
-  top: "-340px"
+  top: "-390px"
 
 }, 1000, 'swing', () => {
   
 })
 
-$('#origins').animate({ left: "10%"}, 1000, 'swing',() => {
+$('#origins').animate({ left: "10%"}, 300, 'swing',() => {
   $('#abilities').animate({
     left: "10%"
-  }, 900, 'swing',() => {
+  }, 300, 'swing',() => {
     $('#weakness').animate({
       left: "10%"
-    }, 800, 'swing',() => {
+    }, 300, 'swing',() => {
       $('#notable-allies').animate({
         left: "10%"
-      }, 700, 'swing',() => {
+      }, 300, 'swing',() => {
         $('#notable-enemies').animate({
           left: "10%"
-        }, 600, 'swing',() => {
+        }, 300, 'swing',() => {
         
         })
       })
@@ -36,16 +28,67 @@ $('#origins').animate({ left: "10%"}, 1000, 'swing',() => {
 
 
 $('#origins').on('click', () => {
-  // $(".test").css({ display: 'grid' })
-  // $(".test").show()
+  if ( $('#origins-content').is(":hidden") ) {
+    $("#origins-content").css({ display: 'flex' })
+    $("#origins-content").animate({ top: '30px', opacity: 1 }, 400, 'swing')
+  } else {
+    $("#origins-content").animate({ top: '0px', opacity: 0 }, 400, 'swing',
+      () => {
+        $("#origins-content").css({ display: 'none' })
+      }
+    )
+  }
 })
 
+$('#abilities').on('click', () => {
+  if ( $('#abilities-content').is(":hidden") ) {
+    $("#abilities-content").css({ display: 'flex' })
+    $("#abilities-content").animate({ top: '30px', opacity: 1 }, 400, 'swing')
+  } else {
+    $("#abilities-content").animate({ top: '0px', opacity: 0 }, 400, 'swing',
+      () => {
+        $("#abilities-content").css({ display: 'none' })
+      }
+    )
+  }
+})
 
-// $('#div1').animate({
-//   marginRight: "100px"
-// }, 500, 'swing', () => {
+$('#weakness').on('click', () => {
+  if ( $('#weakness-content').is(":hidden") ) {
+    $("#weakness-content").css({ display: 'flex' })
+    $("#weakness-content").animate({ top: '30px', opacity: 1 }, 400, 'swing')
+  } else {
+    $("#weakness-content").animate({ top: '0px', opacity: 0 }, 400, 'swing',
+      () => {
+        $("#weakness-content").css({ display: 'none' })
+      }
+    )
+  }
+})
 
-//   $('#div1').css({
-//     backgroundColor: "black"
-//   })
-// })
+$('#notable-allies').on('click', () => {
+  if ( $('#notable-allies-content').is(":hidden") ) {
+    $("#notable-allies-content").css({ display: 'flex' })
+    $("#notable-allies-content").animate({ top: '30px', opacity: 1 }, 400, 'swing')
+  } else {
+    $("#notable-allies-content").animate({ top: '0px', opacity: 0 }, 400, 'swing',
+      () => {
+        $("#notable-allies-content").css({ display: 'none' })
+      }
+    )
+  }
+})
+
+$('#notable-enemies').on('click', () => {
+  if ( $('#notable-enemies-content').is(":hidden") ) {
+    $("#notable-enemies-content").css({ display: 'flex' })
+    $("#notable-enemies-content").animate({ top: '30px', opacity: 1 }, 400, 'swing')
+  } else {
+    $("#notable-enemies-content").animate({ top: '0px', opacity: 0 }, 400, 'swing',
+      () => {
+        $("#notable-enemies-content").css({ display: 'none' })
+      }
+    )
+  }
+})
+x 
